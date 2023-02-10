86d3560715de5e4a982ef6862f7fc30e4e8ca459
md manzer alam <tooshort9541@gmail.com>
2023-02-10 17:09:46 +0530

    added countdown on homeapge

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class EventDates(models.Model):
+    event_start = models.DateTimeField()
+    event_end = models.DateTimeField()
+    type = models.CharField(max_length=100,default="home");
+    name = models.CharField(default="",max_length=100)
+
+    def __str__(self) -> str:
+        return self.name + ' starts at : '+self.event_start.isoformat() + ' ends at : '+self.event_end.isoformat()
--- base/views.py
+++ base/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+from base.models import *
+from django.utils import timezone
 # Create your views here.
 def home(request):
     context = {}
+    event_dates = EventDates.objects.filter(type="home").order_by('-event_start').first()
+    # print(event_dates)
+    msg = "Esummit has ended"
+    time_diff = -1
+    end = True
+    if event_dates is not None:
+        current_time = timezone.now()
+        time_diff = 0
+        end = False
+        if event_dates.event_start > current_time:
+            # hunt is yet to begin
+            time_diff = (event_dates.event_start - current_time).total_seconds()
+            msg = "E-summit Starts in"
+        elif event_dates.event_end > current_time:
+            time_diff = (event_dates.event_end - current_time).total_seconds()
+            msg = "E-summit Ends in"
+        else:
+            time_diff = (event_dates.event_end - current_time).total_seconds()
+            msg = "E-summit has ended"
+            end = True
+    context['msg'] = msg
+    context['time_diff'] = int(time_diff)
+    context['has_ended'] = end
     return render(request,'base/home.html',context)
