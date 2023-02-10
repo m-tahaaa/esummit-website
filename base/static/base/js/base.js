83b4d630a115158010f0e15a10f9548c846070e3
md manzer alam <tooshort9541@gmail.com>
2023-02-10 10:59:34 +0530

    added home page

---
+++ base/static/base/js/base.js
@@ -0,0 +1,29 @@
+function closeMsg(element) {
+    element.parentElement.parentElement.style.display = 'none';
+}
+
+function closeAll() {
+    let msgElement = document.getElementById('msg-element');
+    if (msgElement) {
+        msgElement.style.display = "none";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const myTimeout = setTimeout(closeAll, 4500);
+    // console.log('Set all messages to closed');
+    const faviconTag = document.getElementById("faviconTag");
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const changeFavicon = () => {
+        if(isDark.matches){
+            faviconTag.href = "/static/base/images/favicon_dark.ico";
+        }
+        else {
+            
+            faviconTag.href = "/static/base/images/favicon.ico";
+        }
+    };
+    changeFavicon();
+    setInterval(changeFavicon,1000)
+});
