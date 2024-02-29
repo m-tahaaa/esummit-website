3fd8e8ac828429c31930b8c762a60ed4ca03677e
Rishav Sharma <sharmarishav676@gmail.com>
2024-02-29 16:41:46 +0530

    about section created

--- base/static/base/js/home.js
+++ base/static/base/js/home.js
@@ -4,13 +4,45 @@ const tl = gsap.timeline({
         trigger: "#hero",
         start: "top top",
         end: "bottom top",
-        scrub: true
+        scrub: 0.2
     }
 });
+const sectionTrigger = {
+    trigger: ".about-section",
+    start: "top 40%",
+    end: "top 30%",
+    scrub: 1,
+    markers: false
+}
+// const tl2 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".about-section",
+//         start: "top 60%",
+//         end: "top 50%",
+//         scrub: true ,
+//         markers: true
+//     }
+// });
 
 gsap.utils.toArray(".layer").forEach(layer => {
     const depth = layer.dataset.depth;
     const scale = layer.dataset.scale;
     const movement = -(layer.offsetHeight * depth)
-    tl.to(layer, { y: movement, ease: "none",scale:scale }, 0)
+    tl.to(layer, { y: movement, ease: "none", scale:scale, duration: .2}, 0)
 });
+
+
+gsap.from(".lamp",{
+    scrollTrigger: sectionTrigger,
+    scale: .5,
+    opacity: .2,
+    y: 200,
+    duration: .3
+})
+
+gsap.from(".about-content",{
+    scrollTrigger: sectionTrigger,
+    opacity: 0,
+    y: 50,
+    duration: .3
+})
