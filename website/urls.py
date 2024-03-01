e401152916071f91087496e0524ea198580e5efa
Rishav Sharma <sharmarishav676@gmail.com>
2024-03-02 02:32:01 +0530

    success page added and form bug fixed

--- website/urls.py
+++ website/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', include('base.urls')),
     path('gamers/', include('gamers.urls')),
@@ -23,4 +24,4 @@ urlpatterns = [
     path('quiz/', include('quiz.urls')),
     path('accounts/',include('allauth.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
